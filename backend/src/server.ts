59e5c3819dc75c2908e6ba3fc8228232681a3eeb
Ghenoo <lucasgheno600@gmail.com>
2024-10-25 12:03:19 -0300

    chore: create initialized BD

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,16 @@
+import 'dotenv/config'; 
 import express from 'express';
 import { router as apiRouter } from './routes/api';
+import { connectDb, createUserTable } from './models/userModel';
 
 const app = express();
-
 app.use(express.json());
+
+connectDb().then(() => {
+  createUserTable();
+  console.log("Database connected and user table created.");
+});
+
 app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 5000;
